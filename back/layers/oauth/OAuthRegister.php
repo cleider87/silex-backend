2d952817d26ff4109fb3b9fd08da70ce5dae6250
Cleider <cleider87@gmail.com>
2017-09-14 00:07:08 -0300

    UPDATE reestructuración

---
+++ back/layers/oauth/OAuthRegister.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ *  Silex Backend
+ * 
+ * Copyright (C) 2017
+ * Ing. Cleider R. Herrera M. - cleider87 en gmail.com
+ *
+ * Este programa es software libre; Usted puede usarlo bajo los terminos de la
+ * licencia de software GPL version 2.0 de la Free Software Foundation.
+ *
+ * Este programa se distribuye con la esperanza de que sea util, pero SIN
+ * NINGUNA GARANTIA; tampoco las implicitas garantias de MERCANTILIDAD o
+ * ADECUACION A UN PROPOSITO PARTICULAR.
+ * Consulte la licencia GPL para mas detalles. Usted debe recibir una copia
+ * de la GPL junto con este programa; si no, escriba a la Free Software
+ * Foundation Inc. 51 Franklin Street,5 Piso, Boston, MA 02110-1301, USA.
+ */
+
+namespace App\Business;
+
+/**
+ * Description of BusinessRegister
+ *
+ * @author cleider
+ */
+class OAuthRegister {
+    //put your code here
+}
